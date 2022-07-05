69d881f271f62e108601580dd292af3a0b6d0afb
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-07-05 14:28:05 +0200

    First commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  // NOTE: This is necessary so we don't ship test code to production
+  define: {
+    "import.meta.vitest": "undefined",
+  },
+  test: {
+    // NOTE: This will look for tests in all source files instead of just looking for .test.js/.test.ts.
+    // We also need to include the following to tsconfig.json
+    // "types": [
+    //     "vitest/importMeta"
+    // ]
+    includeSource: ["src/**/*.{js,ts}"],
+  },
+});
